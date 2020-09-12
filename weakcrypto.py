66717c086156311f9c4ef9e324d7298de6b74aec
Flongy <craftbymax@gmail.com>
2020-09-12 22:10:43 +0300

    MultiAlphabet Cipher implementation + minor tweaks

--- weakcrypto.py
+++ weakcrypto.py
@@ -1,5 +1,22 @@
+import random
+
+
+ORD_A = ord('a')
+ORD_Z = ord('z')
+
+
 class BaseCrypto:
     """ Base class. All algorithms have to implement encrypt and decrypt methods """
+    @staticmethod
+    def _encode_text(text: str):
+        """ Convert string var into a list of characters as ints """
+        return list(text.lower().encode("ascii"))
+
+    @staticmethod
+    def _decode_text(lst):
+        """ Convert list of ints (characters) to string """
+        return bytes(lst).decode('ascii')
+
     def encrypt(self, text: str):
         pass
 
@@ -14,43 +31,92 @@ class ShiftCrypto(BaseCrypto):
 
     def __rshift_character(self, character: int):
         """ Shift character RIGHT (overflowing characters cycle over starting with an 'a') """
-        if 97 <= character <= 122:      # is alpha - between symbols 'a' (= 97) and 'z' (= 122)
+        if ORD_A <= character <= ORD_Z:      # is alpha - between symbols 'a' (= 97) and 'z' (= 122)
             character += self.shift
-            if character > 122:         # overflow (more than 'z')
+            if character > ORD_Z:         # overflow (more than 'z')
                 character -= 25
         return character
 
     def __lshift_character(self, character: int):
         """ Shift character LEFT (overflowing characters cycle over starting with a 'z') """
-        if 97 <= character <= 122:      # is alpha - between symbols 'a' (= 97) and 'z' (= 122)
+        if ORD_A <= character <= ORD_Z:      # is alpha - between symbols 'a' (= 97) and 'z' (= 122)
             character -= self.shift
-            if character < 97:          # overflow (less than 'a')
+            if character < ORD_A:          # overflow (less than 'a')
                 character += 25
         return character
 
-    @staticmethod
-    def encode_text(text: str):
-        """ Convert string var into a list of characters as ints """
-        return tuple(text.lower().encode("ascii"))
-
-    @staticmethod
-    def decode_text(lst):
-        """ Convert list of ints (characters) to string """
-        return bytes(lst).decode('ascii')
-
     def encrypt(self, text: str):
         """ Encrypt text into ciphertext """
-        char_as_int_list = self.encode_text(text)
+        char_as_int_list = self._encode_text(text)
         shifted_char_list = map(self.__rshift_character, char_as_int_list)
-        ciphertext = self.decode_text(shifted_char_list)
+        ciphertext = self._decode_text(shifted_char_list)
         return ciphertext
 
     def decrypt(self, ciphertext):
         """ Decrypt ciphertext into plain text """
-        char_as_int_list = self.encode_text(ciphertext)
+        char_as_int_list = self._encode_text(ciphertext)
         shifted_char_list = map(self.__lshift_character, char_as_int_list)
-        text = self.decode_text(shifted_char_list)
+        text = self._decode_text(shifted_char_list)
         return text
+
+
+class MultiAlphabetCrypto(BaseCrypto):
+    """ MultiAlphabet Cipher
+    Using table with 16 randomly picked shift combinations """
+    def __init__(self, seed: int = 42):
+        self.seed = seed
+        if self.seed is not None:
+            random.seed(self.seed)
+
+        table = []
+        for i in range(16):
+            table.append(list(range(26)))
+            random.shuffle(table[i])
+        self.table = table
+
+    @classmethod
+    def from_table(cls, table: list):
+        """ Construct a Crypto Object with the table of shifts """
+        crypto = cls()
+        crypto.table = table
+        return crypto
+
+    def encrypt(self, text: str):
+        """ Encrypt text into ciphertext """
+        char_list = self._encode_text(text)
+
+        i = 0
+        l = len(text)
+        while True:
+            for line in self.table:
+                while not ORD_A <= char_list[i] <= ORD_Z:
+                    i += 1
+                    if i >= l:
+                        return self._decode_text(char_list)
+                char_list[i] = ORD_A + line[char_list[i] - ORD_A]
+
+                i += 1
+                if i >= l:
+                    return self._decode_text(char_list)
+
+    def decrypt(self, ciphertext: str):
+        """ Decrypt ciphertext into plain text """
+        char_list = self._encode_text(ciphertext)
+
+        i = 0
+        l = len(ciphertext)
+        while True:
+            for line in self.table:
+                while not ORD_A <= char_list[i] <= ORD_Z:
+                    i += 1
+                    if i >= l:
+                        return self._decode_text(char_list)
+                # TODO (optimization): remake table for decryption to not use .index
+                char_list[i] = ORD_A + line.index(char_list[i] - ORD_A)
+
+                i += 1
+                if i >= l:
+                    return self._decode_text(char_list)
 
 
 if __name__ == "__main__":
@@ -64,3 +130,21 @@ if __name__ == "__main__":
     crypto = ShiftCrypto(3)
     assert test_string == crypto.decrypt(crypto.encrypt(test_string)), "Shift cipher"
     print("[SUCCESS] Shift Cipher")
+
+    # Testing Multi Alphabet Cipher
+    crypto = MultiAlphabetCrypto()      # Default seed - 42
+    table = crypto.table
+    assert test_string == crypto.decrypt(crypto.encrypt(test_string)), "MultiAlphabet cipher"
+
+    crypto = MultiAlphabetCrypto(1337)  # Different seed
+    assert test_string == crypto.decrypt(crypto.encrypt(test_string)), "MultiAlphabet cipher"
+
+    crypto = MultiAlphabetCrypto.from_table(table)  # Constructed from table
+    assert test_string == crypto.decrypt(crypto.encrypt(test_string)), "MultiAlphabet cipher"
+
+    crypto = MultiAlphabetCrypto(1)
+    encrypted = crypto.encrypt(test_string)
+    crypto = MultiAlphabetCrypto(1)
+    decrypted = crypto.decrypt(encrypted)
+    assert test_string == decrypted, "MultiAlphabet cipher"     # Testing recreated Crypto object
+    print("[SUCCESS] MultiAlphabet Cipher")
